84bf4d9f1c0ed5dfc0e6558b1f9792cda1ac1ae0
SOL-ENE <solene@amoussou.org>
2026-05-06 21:26:23 +0200

    feat: amélioration de la connexion à la BDD avec création de pool et gestion des paramètres d'URL

--- server/db.js
+++ server/db.js
@@ -1,9 +1,16 @@
 import mysql from 'mysql2/promise'
 
-if (!process.env.DB_URL) {
-  throw new Error('DB_URL is not defined in environment variables')
-}
+const dbUrl = new URL(process.env.DB_URL)
 
-const pool = mysql.createPool(process.env.DB_URL)
+const pool = mysql.createPool({
+  host: dbUrl.hostname,
+  port: parseInt(dbUrl.port) || 4000,
+  user: decodeURIComponent(dbUrl.username),
+  password: decodeURIComponent(dbUrl.password),
+  database: dbUrl.pathname.slice(1),
+  ssl: { rejectUnauthorized: false },
+  waitForConnections: true,
+  connectionLimit: 5,
+})
 
 export default pool
