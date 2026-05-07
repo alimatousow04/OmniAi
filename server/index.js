741fc9a3f2b32397137fc1828f8de7f1e5c57c12
SOL-ENE <solene@amoussou.org>
2026-05-07 04:23:46 +0200

    feat: simplification de la gestion CORS dans le serveur

--- server/index.js
+++ server/index.js
@@ -11,13 +11,14 @@ import pool from './db.js'
 dotenv.config()
 
 const app = express()
-// app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173' }))
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    process.env.FRONTEND_URL || 'http://localhost:5173'
-  ]
-}))
+app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173' }))
+// app.use(cors({
+//   origin: [
+//     'http://localhost:5173',
+//     process.env.FRONTEND_URL || 'http://localhost:5173'
+//   ]
+// }))
+
 app.use(express.json())
 
 app.use('/api/auth', authRouter)
